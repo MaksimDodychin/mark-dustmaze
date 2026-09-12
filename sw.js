5cd99b78446c70344a5e0fc13d0cf3a1f9ceb6bb
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 01:38:47 +0300

    Add taller maze walls, soft dust fibers and realistic vacuum details

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'd5b78c86a7';
+const VERSION = '1b7607dd55';
 const PREFIX = "dustmaze-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
