41f8483280060481b501f9668c485849c2f412d4
Максим Добычин <dobyvankenoby@gmail.com>
2026-09-13 12:20:52 +0300

    🔊 Аудиосубтитры: герои говорят вслух своими голосами (задания не читаются)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Scoped offline application updater; version changes with the payload and updater.
-const VERSION = 'e0f6ed81cb';
+const VERSION = '768d72cbd4';
 const PREFIX = "dustmaze-";
 const CACHE = PREFIX + VERSION;
 const SCOPE = new URL(self.registration.scope);
